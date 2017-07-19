aed204a685b119ba1158d922a9a5fceffac063a6
James W. Tonkin <btonkin71@comcast.net>
2017-07-19 13:29:24 -0500

    Exercise complete

--- hashsetexercise/hashsetexercise/Program.cs
+++ hashsetexercise/hashsetexercise/Program.cs
@@ -10,6 +10,62 @@ namespace hashsetexercise
     {
         static void Main(string[] args)
         {
+            HashSet<string> showRoom = new HashSet<string>();
+
+            showRoom.Add("Toyota");
+            showRoom.Add("Nissan");
+            showRoom.Add("Ford");
+            showRoom.Add("Hyundai");
+
+            Console.Write(showRoom.Count);
+
+            showRoom.Add("Ford");
+
+            foreach (string model in showRoom) {
+                Console.Write("\n" + model);
+            }
+
+            HashSet<string> usedLot = new HashSet<string>();
+
+            usedLot.Add("Chevrolet");
+            usedLot.Add("Jeep");
+
+            showRoom.UnionWith(usedLot);
+            foreach (string model in showRoom)
+            {
+                Console.Write("\n" + model);
+            }
+
+            showRoom.Remove("Ford");
+            foreach (string model in showRoom)
+            {
+                Console.Write("\n" + model);
+            }
+
+            HashSet<string> junkYard = new HashSet<string>();
+
+            junkYard.Add("Porsche");
+            junkYard.Add("Ferrari");
+            junkYard.Add("BMW");
+            junkYard.Add("Chevrolet");
+            junkYard.Add("Hyundai");
+
+            showRoom.IntersectWith(junkYard);
+            foreach (string same in showRoom)
+            {
+                Console.Write("\n" + same);
+            }
+
+            showRoom.UnionWith(junkYard);
+            showRoom.Remove("BMW");
+
+            Console.Write("\nMy Cars Now:");
+            foreach (string mycars in showRoom)
+            {
+                Console.Write("\n" + mycars);
+            }
+
+            Console.Read();
         }
     }
 }
